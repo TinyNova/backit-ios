ba91f24c1d1bfdc19630540779647f3ea083fc8f
Eric Chamberlain <eric.chamberlain@hotmail.com>
2018-12-26 15:23:39 -0800

    Initial commit.

---
+++ Backit/Backit/Application/Changelog/ChangelogViewController.swift
@@ -0,0 +1,14 @@
+/**
+ *
+ * Copyright © 2018 Backit. All rights reserved.
+ */
+
+import UIKit
+
+class ChangelogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ Backit/Backit/Application/Homepage/HomepageViewController.swift
@@ -0,0 +1,14 @@
+/**
+ *
+ * Copyright © 2018 Backit. All rights reserved.
+ */
+ 
+import UIKit
+
+class HomepageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
