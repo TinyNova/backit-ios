00d71aab62ffe64b56a84e02aac8e985597756f2
Eric Chamberlain <eric.chamberlain@hotmail.com>
2019-01-24 13:28:16 -0800

    Moved dependencies in respective files.

---
+++ Backit/Backit/Service/Request/ProjectRequest.swift
@@ -0,0 +1,71 @@
+/**
+ *
+ * Copyright © 2018 Backit. All rights reserved.
+ */
+
+import Foundation
+
+struct ProjectRequest: Request {
+    /**
+     Needed:
+     - Comments (number of comments)
+     - Video Preview URL
+     - Video URL (can I use `video`?)
+     - More project images?
+     */
+    
+    /**
+     Image extension info:
+     p = portrait
+     t = thumb
+     c = card
+     */
+    struct ProjectImages: Decodable {
+        let p: String
+        let t: String
+        let c: String
+    }
+    struct Project: Decodable {
+        let projectId: String
+        let site: String
+        let externalId: String
+        let url: String
+        let internalUrl: String
+        let slug: String
+        let country: String
+        let category: String
+        let subCategory: String?
+        let currency: String
+        let name: String
+        let goal: String
+        let pledged: String
+        let backerCount: String
+        let blurb: String
+        let image: ProjectImages
+        let video: String
+        let visible: Bool
+        let funding: Bool
+        let hasEarlyBirdRewards: Bool
+    }
+    
+    struct ResponseType: Decodable {
+        let projects: [ProjectRequest.Project]
+    }
+    
+    enum Parameter {
+        case funding(Bool)
+        case backerCountMin(Int)
+        case country(String)
+        case sort(String)
+        case sortDirection(String)
+        case offset(Int)
+        case limit(Int)
+    }
+    
+    var url = "https://collect.backit.com/projects"
+    var parameters: [Parameter]
+    
+    init(parameters: [Parameter]) {
+        self.parameters = parameters
+    }
+}
