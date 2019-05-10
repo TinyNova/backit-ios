fd349b63e463d07fcd9d8fb2235d4adc4a9c31fd
Eric Chamberlain <eric.chamberlain@hotmail.com>
2019-05-09 21:53:49 -0700

    Connected all outlets to view. App displays empty projects.

--- Backit/Backit/Application/ProjectFeed/ProjectTableViewCell.swift
+++ Backit/Backit/Application/ProjectFeed/ProjectTableViewCell.swift
@@ -3,4 +3,17 @@ import UIKit
 
 class ProjectTableViewCell: UITableViewCell {
     
+    @IBOutlet weak var favoriteImageView: UIImageView!
+    @IBOutlet weak var imagePageIndicator: UIStackView!
+    @IBOutlet weak var imageCarousel: UIStackView!
+    @IBOutlet weak var projectNameLabel: UILabel!
+    @IBOutlet weak var fundedPercentProgressView: UIProgressView!
+    @IBOutlet weak var percentFundedLabel: UILabel!
+    @IBOutlet weak var earlyBirdLabel: UILabel!
+    @IBOutlet weak var daysLeftLabel: UILabel!
+    @IBOutlet weak var separatorView: UIView!
+    @IBOutlet weak var totalVotesLabel: UILabel!
+    @IBOutlet weak var totalCommentsLabel: UILabel!
+    @IBOutlet weak var shareView: UIView!
+    @IBOutlet weak var bottomSpacerView: UIView!
 }
