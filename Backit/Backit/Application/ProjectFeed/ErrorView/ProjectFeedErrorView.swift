c42788a3c04fc45fdf95cf1ff8e44b8912a9f834
Eric Chamberlain <eric.chamberlain@hotmail.com>
2019-05-12 07:01:02 -0700

    Adding the error view

---
+++ Backit/Backit/Application/ProjectFeed/ErrorView/ProjectFeedErrorView.swift
@@ -0,0 +1,33 @@
+import Foundation
+import UIKit
+
+protocol ProjectFeedErrorViewDelegate: class {
+    func didRequestToReloadData()
+}
+
+class ProjectFeedErrorView: UIView {
+    
+    @IBOutlet weak var errorMessageLabel: UILabel!
+    
+    weak var delegate: ProjectFeedErrorViewDelegate?
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configure()
+    }
+    
+    init() {
+        super.init(frame: CGRect.zero)
+        configure()
+    }
+    
+    private func configure() {
+//        fromNib()
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(didTapView))
+        addGestureRecognizer(gesture)
+    }
+    
+    @objc private func didTapView(sender: UITapGestureRecognizer) {
+        delegate?.didRequestToReloadData()
+    }
+}
