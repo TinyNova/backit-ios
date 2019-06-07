595bd63e6e8fca8960e9d042a57063919207a07e
Eric Chamberlain <eric.chamberlain@hotmail.com>
2019-06-07 10:45:03 -0700

    File uploads now work.

--- Backit/Backit/Service/Amazon/AmazonService.swift
+++ Backit/Backit/Service/Amazon/AmazonService.swift
@@ -38,37 +38,34 @@ class AmazonService {
         guard let url = URL(string: "https://\(file.bucket).s3.amazonaws.com/") else {
             return Future(error: .failedToCreateRequest)
         }
-        guard let data = image.jpegData(compressionQuality: 1.0) else {
+        guard let jpegData = image.jpegData(compressionQuality: 1.0) else {
             return Future(error: .failedToConvertImageToJpeg)
         }
         
         let promise = Promise<IgnorableValue, AmazonServiceError>()
 
         let boundary = generateBoundary()
-        let httpBody = generateHTTPBody(for: file, data: data, boundary: boundary)
+        let httpBody = generateHTTPBody(for: file, jpegData: jpegData, boundary: boundary)
         
         let filename = file.key
         
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.allHTTPHeaderFields = [
-            "Content-Type": "multipart/form-data; boundary=\"\(boundary)\"",
-            "Content-Length": "\(httpBody.count)",
+            "Content-Type": "multipart/form-data; boundary=\(boundary)",
+            "Cache-Control": "no-cache"
         ]
         request.httpBody = httpBody
+
         print("INFO: Making request to upload file: \(filename) to bucket: \(file.bucket)")
         let task = urlSession.dataTask(with: request) { (data, response, error) in
-            guard let response = response as? HTTPURLResponse else {
-                promise.failure(.failedToUploadFile(statusCode: 0, error: nil))
-                return
-            }
             if let error = error {
-                promise.failure(.failedToUploadFile(statusCode: response.statusCode, error: error))
-                return print("ERR: Failed to upload w/ status code: \(response.statusCode), error: \(error)")
+                promise.failure(.failedToUploadFile(statusCode: 0, error: error))
+                return print("ERR: Failed to upload w/ error: \(error)")
             }
-            if response.statusCode >= 400 {
+            if let response = response as? HTTPURLResponse, response.statusCode >= 400 {
                 promise.failure(.failedToUploadFile(statusCode: response.statusCode, error: nil))
-                return print("ERR: Failed to upload w/ status code: \(response.statusCode)")
+                return print("ERR: Failed to upload w/ status code: \(response.statusCode) data:\n\(String(data: data ?? Data(), encoding: .utf8)!)")
             }
             
             promise.success(IgnorableValue())
@@ -85,32 +82,44 @@ class AmazonService {
         return value.addingPercentEncoding(withAllowedCharacters: charSet)!
     }
     
-    private func generateHTTPBody(for file: S3UploadFile, data: Data, boundary: String) -> Data {
+    private func generateHTTPBody(for file: S3UploadFile, jpegData: Data, boundary: String) -> Data {
         var httpBody = Data()
         
         func append(_ string: String) {
-            httpBody.append(string.data(using: .utf8, allowLossyConversion: false) ?? Data())
+            guard let data = string.data(using: .utf8, allowLossyConversion: false) else {
+                return print("ERR: Failed to append string \(string) to HTTPBody")
+            }
+            httpBody.append(data)
         }
         
         func appendField(name: String, value: String) {
             append("--\(boundary)\r\n")
-            append("Content-Type: text/plain; charset=utf-8\r\n")
             append("Content-Disposition: form-data; name=\(name)\r\n\r\n")
             append(value)
+            append("\r\n")
+        }
+
+        func appendFile(name: String, fileData: Data, contentType: String) {
+            append("--\(boundary)\r\n")
+            append("Content-Disposition: form-data; name=\(name)\r\n")
+            append("Content-Type: \(contentType)\r\n\r\n")
+            httpBody.append(fileData)
+            append("\r\n")
         }
         
-        // Fields
+        // Field(s)
         appendField(name: "acl", value: file.acl)
         appendField(name: "AWSAccessKeyId", value: file.awsKey)
         appendField(name: "key", value: file.key)
         appendField(name: "policy", value: file.policy)
         appendField(name: "signature", value: file.signature)
+        appendField(name: "Content-Type", value: "image/jpeg")
         
-        // Image
-        append("Content-Type: image/jpeg\r\n")
-        append("Content-Disposition: form-data; name=file\r\n\r\n")
-        httpBody.append(data)
-        append("\r\n--\(boundary)--\r\n")
+        // Image(s)
+        appendFile(name: "file", fileData: jpegData, contentType: "image/jpeg")
+
+        // Finish
+        append("--\(boundary)--")
         
         return httpBody
     }
