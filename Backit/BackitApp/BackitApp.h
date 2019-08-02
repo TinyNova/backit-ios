c9ff1f7262af595ce948c98ea109d55995fe6db9
Eric Chamberlain <eric.chamberlain@backit.com>
2019-08-02 15:42:30 -0700

    the app builds but crashes

---
+++ Backit/BackitApp/BackitApp.h
@@ -0,0 +1,18 @@
+//
+//  BackitApp.h
+//  BackitApp
+//
+//  Created by Backit on 8/2/19.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BackitApp.
+FOUNDATION_EXPORT double BackitAppVersionNumber;
+
+//! Project version string for BackitApp.
+FOUNDATION_EXPORT const unsigned char BackitAppVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BackitApp/PublicHeader.h>
+
+
