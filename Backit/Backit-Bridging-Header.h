d47649de4ef55726dc932c6b116beef5efa942d4
Eric Chamberlain <eric.chamberlain@backit.com>
2019-08-02 09:49:55 -0700

    Got the app running with buck pre-compiled library

--- Backit/Backit-Bridging-Header.h
+++ Backit/Backit-Bridging-Header.h
@@ -2,4 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+//#import "Libraries/BKFoundation-Swift.h"
 #import "NewRelicAgent/NewRelic.h"
