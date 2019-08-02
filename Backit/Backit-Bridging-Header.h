413404600682e44e9e10c0c2ec2d1e346d0174a1
Eric Chamberlain <eric.chamberlain@backit.com>
2019-08-02 16:02:05 -0700

    App now has assets. App still crashes after install.

--- Backit/Backit-Bridging-Header.h
+++ Backit/Backit-Bridging-Header.h
@@ -2,5 +2,4 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-//#import "Libraries/BKFoundation-Swift.h"
 #import "NewRelicAgent/NewRelic.h"
