8ff5f73270c664e80c064fc735d1d765bd9ed531
Eric Chamberlain <eric.chamberlain@hotmail.com>
2019-08-27 17:45:08 -0700

    Project description complete.

--- Backit/Application/ProjectDetails/ProjectDescriptionViewController.swift
+++ Backit/Application/ProjectDetails/ProjectDescriptionViewController.swift
@@ -9,7 +9,28 @@ class ProjectDescriptionViewController: UIViewController {
     private var htmlString: String?
     
     public func configureWith(htmlString: String) {
-        self.htmlString = htmlString
+        let removeNewlines = htmlString.replacingOccurrences(of: "\\n", with: "<br/>")
+        self.htmlString = """
+        <html>
+        <head>
+        <title>Project Description</title>
+        <style>
+        body {
+          color: #020621;
+          font: 20pt "Helvetica Neue", Helvetica, Arial, "Liberation Sans", FreeSans, sans-serif;
+          -webkit-font-smoothing: antialiased;
+          margin: auto;
+          padding: 10pt;
+          text-align: center;
+        
+        }
+        </style>
+        </head>
+        <body>
+        \(removeNewlines)
+        </body>
+        </html>
+        """
     }
     
     override func viewDidLoad() {
